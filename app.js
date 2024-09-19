24b453e0c7774af2ddef1a28f1f8286201a482b6
Devrim Savas <137702797+devrimsavas@users.noreply.github.com>
2024-09-19 02:14:11 +0200

    added

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files (CSS, JS, images) from 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
+
+// Set up route for serving static HTML files from 'view' directory
 app.use(express.static(path.join(__dirname, "view")));
 
 const port = 3000;
 
-app
-  .get("/", (req, res) => {
-    return res.status(200).json({
-      message: "hello world",
-    });
-  })
-  .listen(port, () => {
-    console.log(`listening at port ${port}`);
-  });
+// Route for the root path ("/"), rendering the balls.html page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "balls.html")); // Serve balls.html
+});
+
+// Route for the "/game" path, rendering the game.html page
+app.get("/game", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "game.html")); // Serve game.html
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
